191935ceb4aee7f3b0dd5dbf6aab3622b2b1760e
Jonathan Guthrie <jono@perfect.org>
2016-07-09 17:07:14 -0400

    Updated name of linux modules

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,6 @@
 import XCTest
-@testable import Perfect-CURLTestSuite
+@testable import PerfectCURLTestSuite
 
 XCTMain([
-	testCase(Perfect-CURLTests.allTests),
+	testCase(PerfectCURLTests.allTests),
 ])
