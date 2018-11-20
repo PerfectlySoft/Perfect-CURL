a9f8950554585bd12b9ec24ad4bfbe0b1519e03f
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-11-20 11:14:32 -0500

    updated package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
+// swift-tools-version:4.0
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-09-20 19:18:02 +0000
 import PackageDescription
 let package = Package(
 	name: "PerfectCURL",
-	targets: [],
+	products: [.library(name: "PerfectCURL", targets: ["PerfectCURL"])],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", from: "2.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "PerfectCURL", dependencies: ["PerfectHTTP"]),
+		.testTarget(name: "PerfectCURLTests", dependencies: ["PerfectHTTP", "PerfectCURL"])
 	]
 )
